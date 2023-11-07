f0c10eedabaf301a87cabe56fcda27eae4fcbf4e
Bene <b.rey@arcware.com>
2023-11-07 20:44:53 +0100

    Added tests for flatten

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,47 @@
 type BaseType = number | string | boolean;
 type GenericObject<T = any> = Record<string, T>;
 
-export function flatten<Obj extends GenericObject>(obj: Obj, path = "") {
+/**
+ * Recursively flattens a nested object with keys as paths to the values.
+ *
+ * @param {GenericObject} obj - The object to flatten.
+ * @param {string} path - The initial path for keys. Used during recursion.
+ * @returns {GenericObject<BaseType>} A new object with no nested properties.
+ */
+export function flatten<Obj extends GenericObject>(
+  obj: Obj,
+  path = ""
+): GenericObject<BaseType> {
+  const type = typeof obj;
+  if (
+    obj === null ||
+    obj === undefined ||
+    ["string", "number", "boolean"].includes(type)
+  )
+    throw new TypeError(
+      `flatten() Unexpected type '${
+        obj === null ? null : obj === undefined ? undefined : type
+      }' for input.`
+    );
+
+  // Retrieve the keys of the object
   const keys = Object.keys(obj);
 
+  // Initialize the result object
   const result: GenericObject<BaseType> = {};
 
-  for (let key in keys) {
-    const newKey = path.length ? `${path}.${key}` : key;
+  for (let key of keys) {
+    // Changed 'in' to 'of' to correctly iterate over keys
+    // Construct the new key based on the path
+    const navigatePath = path.length ? `${path}.${key}` : key;
 
+    // Check if the value is an object and not null
     if (typeof obj[key] === "object" && obj[key] !== null) {
-      Object.assign(result, flatten(obj[key]), newKey);
+      // Recursively flatten the nested object
+      Object.assign(result, flatten(obj[key] as any, navigatePath));
     } else {
-      result[newKey] = obj[key];
+      // Assign the value to the navigatePath in the result object
+      result[navigatePath] = obj[key];
     }
   }
 
---
+++ test/flatten.test.ts
@@ -0,0 +1,109 @@
+import { flatten } from "../src/utils";
+
+describe("flatten", () => {
+  it("should flatten an object with nested objects", () => {
+    const nestedObj = {
+      a: 1,
+      b: { c: 2, d: { e: 3 } },
+      f: true,
+    };
+    const flatObj = flatten(nestedObj);
+    expect(flatObj).toEqual({
+      a: 1,
+      "b.c": 2,
+      "b.d.e": 3,
+      f: true,
+    });
+  });
+
+  it("should handle an already flat object", () => {
+    const flatObj = { a: 1, b: 2, c: true };
+    expect(flatten(flatObj)).toEqual(flatObj);
+  });
+
+  it("should handle an empty object", () => {
+    expect(flatten({})).toEqual({});
+  });
+
+  it("should correctly handle different types of values", () => {
+    const mixedObj = {
+      a: "hello",
+      b: { c: null, d: false },
+      e: [1, 2, 3], // Arrays are objects and would be a special case, should be considered based on requirements
+    };
+    const expectedResult = {
+      a: "hello",
+      "b.c": null, // assuming null should be flattened as well
+      "b.d": false,
+      "e.0": 1,
+      "e.1": 2,
+      "e.2": 3,
+    };
+    expect(flatten(mixedObj)).toEqual(expectedResult);
+  });
+
+  it("should handle a straight array as input", () => {
+    const arrayInput = [1, 2, { a: 3, b: [4, 5] }];
+    const expectedOutput = {
+      "0": 1,
+      "1": 2,
+      "2.a": 3,
+      "2.b.0": 4,
+      "2.b.1": 5,
+    };
+    expect(flatten(arrayInput as any)).toEqual(expectedOutput);
+  });
+
+  it("should flatten an object containing arrays", () => {
+    const objWithArrays = {
+      numbers: [1, 2, 3],
+      details: {
+        name: "Test",
+        values: [4, 5, 6],
+      },
+    };
+    const expectedOutput = {
+      "numbers.0": 1,
+      "numbers.1": 2,
+      "numbers.2": 3,
+      "details.name": "Test",
+      "details.values.0": 4,
+      "details.values.1": 5,
+      "details.values.2": 6,
+    };
+    expect(flatten(objWithArrays)).toEqual(expectedOutput);
+  });
+
+  it("should throw an error for string input", () => {
+    const stringInput = "test";
+    expect(() => flatten(stringInput as any)).toThrow();
+  });
+
+  it("should throw an error for number input", () => {
+    const numberInput = 42;
+    expect(() => flatten(numberInput as any)).toThrow();
+  });
+
+  it("should throw an error for boolean input", () => {
+    const booleanInput = true;
+    expect(() => flatten(booleanInput as any)).toThrow();
+  });
+
+  it("should throw an error for null input", () => {
+    expect(() => flatten(null as any)).toThrow();
+  });
+
+  it("should throw an error for undefined input", () => {
+    expect(() => flatten(undefined as any)).toThrow();
+  });
+
+  it("should not throw an error for an object input", () => {
+    const objectInput = { a: 1, b: { c: 2 } };
+    expect(() => flatten(objectInput)).not.toThrow();
+  });
+
+  it("should not throw an error for an array input", () => {
+    const arrayInput = [1, 2, 3];
+    expect(() => flatten(arrayInput as any)).not.toThrow();
+  });
+});
