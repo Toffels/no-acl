b52de523ce7bc136398198c474dbb691d0d98494
Bene <b.rey@arcware.com>
2023-11-12 17:14:17 +0100

    setup webpack and web working stuffs

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { ACL } from "./ACL";
 export { UserACL } from "./UserACL";
 export * as utils from "./utils/utils";
 export type { DeepPartial } from "./utils/DeepPartial";
+export type { SimpleDescriptor } from "./Types";
+export { SimpleDescriptorEnum, SDE } from "./Types";
---
+++ src/zod/ZodAcl.ts
@@ -0,0 +1,3 @@
+import * as z from "zod";
+
+console.log("hi");
